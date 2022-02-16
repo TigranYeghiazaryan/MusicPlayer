e49d3e5c4cd33285e0eef9d3fa24f94297596478
TigranCodes <97300572+DevJsTYeghiazaryan@users.noreply.github.com>
2022-02-16 20:57:45 +0400

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,39 @@
+let allMusic = [
+    {
+      name: "Billie Eilish - Therefore I Am",
+      artist: "Billie Eilish",
+      img: "1m", 
+      src: "1m"
+    },
+    {
+      name: "(feat. Sean Paul & Anne-Marie) - Rockabye",
+      artist: "Clean Bndit",
+      img: "2m",
+      src: "2m"
+    },
+    {
+      name: "Beauz & Jvna - Crazy",
+      artist: "Beauz & Jvna",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "Hardwind - Want Me",
+      artist: "Mike Archangelo",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "Jim - Sun Goes Down",
+      artist: "Jim Yosef x Roy",
+      img: "music-5",
+      src: "music-5"
+    },
+    {
+      name: "Lost Sky - Vision NCS",
+      artist: "NCS Release",
+      img: "music-6",
+      src: "music-6"
+    },
+
+  ];
